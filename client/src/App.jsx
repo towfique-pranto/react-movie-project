33ff8a098be062a6c242899193ba91c9f6b85160
Towfique Kabir <towfique.pranto@gmail.com>
2026-02-25 00:15:50 +0600

    feat: add routing, Home, and Favorites pages.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,21 @@
 import Home from './pages/Home'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
+import Favorites from './pages/Favorites';
+import NavBar from "./components/NavBar";
 
 function App() {
-
-
   return (
-    <>
-      <Home />
-    </>
-
+    <div>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
-
-
 
 export default App
