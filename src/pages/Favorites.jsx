5a4c8543e7e31f2cbd2ac42323e6876da54c56fa
Towfique Kabir <towfique.pranto@gmail.com>
2026-02-27 01:52:18 +0600

    feat: add option to bookmark movies in favorites

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,11 +1,28 @@
 import "../css/Favorites.css";
+import { useMovieContext } from "../contexts/MovieContext";
+import MovieCard from "../components/MovieCard";
 
 function Favorites() {
-    return (
-        <div>
-            <h1>Favorites</h1>
-        </div>
-    );
-}
+  const { favorites } = useMovieContext();
 
-export default Favorites
+  if (favorites.length > 0) {
+    return (
+      <div className="favorites">
+        <h2>Favorites</h2>
+        <div className="movies-grid">
+          {favorites.map((movie) => {
+            return <MovieCard movie={movie} key={movie.id} />;
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="favorites-empty">
+        <h2>No Favorite Movie Added</h2>
+        <p>Start adding favorites to show them here</p>
+      </div>
+    );
+  }
+}
+export default Favorites;
