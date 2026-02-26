0e7e876e8cbc1260dae607b8c57382fb3d2688ff
Towfique Kabir <towfique.pranto@gmail.com>
2026-02-26 17:19:19 +0600

    feat: add styling to all the components and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import Home from './pages/Home'
 import { Routes, Route } from 'react-router-dom'
-import './App.css'
+import './css/App.css'
 import Favorites from './pages/Favorites';
 import NavBar from "./components/NavBar";
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import MovieCard from "../components/MovieCard"
 import { useState } from "react";
-
+import "../css/Home.css";
 
 function Home() {
     const [searchTerm, setSearchTerm] = useState("");
